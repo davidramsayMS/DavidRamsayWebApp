435ac97ef6e859e023d5a2cade53311814e566d0
davidramsayMS <davidramsay@microsoft.com>
2023-12-11 09:01:09 -0500

    Add project files.

---
+++ Pages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DavidRamsayWebApp20230801.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Blog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DavidRamsayWebApp20230801.Pages
+{
+    public class BlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
